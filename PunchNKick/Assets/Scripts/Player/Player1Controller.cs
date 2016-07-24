2366f5cdb5d20004bb24a320f63822a623be8c75
R Pringle <reubenpringlenz@gmail.com>
2016-07-24 21:22:58 +1200

    Simplified Player1Controller Code

--- PunchNKick/Assets/Scripts/Player/Player1Controller.cs
+++ PunchNKick/Assets/Scripts/Player/Player1Controller.cs
@@ -5,16 +5,13 @@ public class Player1Controller : MonoBehaviour
 	//public int m_PlayerNumber = 1;         
 	public float speed = 12f;                 
 	   
-	private Rigidbody2D rb;         
-	private float m_MovementInputValue;
+	private Rigidbody rb;         
 
 
 
 	private void Start()
 	{
-		rb = GetComponent<Rigidbody2D> ();
-		//m_MovementAxisName  = "Horizontal" + m_PlayerNumber;
-		//m_MovementAxisName = "Vertical" + m_PlayerNumber;
+		rb = GetComponent<Rigidbody> ();
 	}
 		
 
