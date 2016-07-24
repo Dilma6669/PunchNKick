8ac5a05c04f57511855859448da7a153f02091bc
Dpringle <pridy274@student.otago.ac.nz>
2016-07-24 21:05:56 +1200

    Dylan... Changed player movement

--- PunchNKick/Assets/Scripts/Player/Player1Controller.cs
+++ PunchNKick/Assets/Scripts/Player/Player1Controller.cs
@@ -2,57 +2,32 @@
 
 public class Player1Controller : MonoBehaviour
 {
-	public int m_PlayerNumber = 1;         
-	public float m_Speed = 12f;                 
-
-	private string m_MovementAxisName;   
-	private Rigidbody m_Rigidbody;         
+	//public int m_PlayerNumber = 1;         
+	public float speed = 12f;                 
+	   
+	private Rigidbody2D rb;         
 	private float m_MovementInputValue;
 
-
-	private void Awake()
-	{
-		m_Rigidbody = GetComponent<Rigidbody>();
-	}
-
-
-	private void OnEnable ()
-	{
-		m_Rigidbody.isKinematic = false;
-		m_MovementInputValue = 0f;
-	}
-
-
-	private void OnDisable ()
-	{
-		m_Rigidbody.isKinematic = true;
-	}
 
 
 	private void Start()
 	{
-		m_MovementAxisName  = "Horizontal" + m_PlayerNumber;
-		m_MovementAxisName = "Vertical" + m_PlayerNumber;
+		rb = GetComponent<Rigidbody2D> ();
+		//m_MovementAxisName  = "Horizontal" + m_PlayerNumber;
+		//m_MovementAxisName = "Vertical" + m_PlayerNumber;
 	}
 		
-	private void Update()
-	{
-		m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
-	}
 
 	private void FixedUpdate()
 	{
-		// Move and turn the tank.
-		Move ();
+
+		float moveHorizontal = Input.GetAxis ("Horizontal1");
+		float moveVertical = Input.GetAxis ("Vertical1");
+
+		transform.Translate( new Vector3(Time.deltaTime * speed * -moveHorizontal,0,0));
+		transform.Translate( new Vector3(0,0,Time.deltaTime * speed * -moveVertical));
 	}
 
 
-	private void Move()
-	{
-		// Adjust the position of the tank based on the player's input.
-		Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
 
-		m_Rigidbody.MovePosition (m_Rigidbody.position + movement);
-	}
 }
-//            movement = movement.normalized * speed * Time.deltaTime;
