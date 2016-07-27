3c2285b55635530c0c603f65c5a76e56cd0a28c5
R Pringle <reubenpringlenz@gmail.com>
2016-07-27 14:49:10 +1200

    REUBEN... Player 1 can now turn either right or left.

--- PunchNKick/Assets/Scripts/Player/Player1Controller.cs
+++ PunchNKick/Assets/Scripts/Player/Player1Controller.cs
@@ -5,12 +5,16 @@ public class Player1Controller : MonoBehaviour
 	//public int m_PlayerNumber = 1;         
 	public float speed = 12f;                 
 	   
-	private Rigidbody rb;         
+	private Rigidbody rb;
+
+	private bool facingRight;
+	//player is facing right instead of left.
 
 
 
 	private void Start()
 	{
+		facingRight = true;
 		rb = GetComponent<Rigidbody> ();
 	}
 		
@@ -23,7 +27,27 @@ public class Player1Controller : MonoBehaviour
 
 		transform.Translate( new Vector3(Time.deltaTime * speed * -moveHorizontal,0,0));
 		transform.Translate( new Vector3(0,0,Time.deltaTime * speed * -moveVertical));
+
+		Turn (moveHorizontal);
 	}
+
+	private void Turn(float moveHorizontal)
+	//code for turning the player to either right or left.
+	{
+		if (moveHorizontal > 0 && !facingRight || moveHorizontal < 0 && facingRight) 
+		{
+			facingRight = !facingRight;
+
+			Vector3 playerScale = transform.localScale;
+
+			playerScale.x *= -1;
+
+			transform.localScale = playerScale;
+
+		}
+	}
+
+
 
 
 
