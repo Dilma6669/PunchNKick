a93723e25a80042e5aeec3974e1772371f7871e2
R Pringle <reubenpringlenz@gmail.com>
2016-07-30 03:10:07 +1200

    REUBEN.... Unticked X and Y rotation axis to stop player "falling over". Running and Jumping animation implemented. Problems that need addressing: Running: Speed does not increase by a multiple of say 2. Jumping: A half made script to elevate the player and then fall back down due to gravity, but the player controller within the inspector is causing conflicts with the vector 3 movement within the script so it is disabled plus I have // the movement code for the y axis as I don't think it's right.

--- PunchNKick/Assets/Scripts/Player/Player1Controller.cs
+++ PunchNKick/Assets/Scripts/Player/Player1Controller.cs
@@ -3,8 +3,15 @@
 public class Player1Controller : MonoBehaviour
 {
 	//public int m_PlayerNumber = 1;         
-	public float speed = 5f;                 
-	   
+	public float speed = 5f;   
+
+	private CharacterController player1Control;
+
+	private bool run;
+	private float verticalVelocity;
+	private float gravity = 14f;
+	private float jumpForce = 10f;
+
 	private Animator anim;
 
 	//player is facing right instead of left.
@@ -14,39 +21,89 @@ public class Player1Controller : MonoBehaviour
 
 	private void Start()
 	{
-		facingRight = true;
+		player1Control = GetComponent <CharacterController>();
+		run = false;
 		anim = GetComponent<Animator> ();
+		facingRight = true;
 	}
 		
+	private void Update()
+	{
+		//Sets Running animation **NEED TO INCREASE SPEED BUT EVERYTHING I'VE TRIED DIDN'T WORK PROPERLY**
+		if (Input.GetKey(KeyCode.LeftShift)) 
+		{
+			run = true;
+		} 
+		else 
+		{
+			run = false;
+		}
+
+		//Sets Jumping animation
+		if (Input.GetKey(KeyCode.Space)) 
+		{
+			anim.SetBool ("Jumping", true);
+		} 
+		else 
+		{
+			anim.SetBool ("Jumping", false);
+		}
+
+
+	}
 
 
 	private void FixedUpdate()
 	{
 		// Basic Movement Player //
 		float moveHorizontal = Input.GetAxis ("Horizontal1");
+		//*************float moveY = verticalVelocity;
 		float moveVertical = Input.GetAxis ("Vertical1");
 
 		//Sets x and y basic movement
 		transform.Translate (new Vector3 (Time.deltaTime * speed * moveHorizontal, 0, 0));
+		//**********transform.Translate (new Vector3 (0, Time.deltaTime * moveY, 0));
 		transform.Translate (new Vector3 (0, 0, Time.deltaTime * speed * moveVertical));
 
 		//code for walking animation to flow fluently between x and z planes
 		anim.SetFloat ("WalkSpeedX", Mathf.Abs (moveHorizontal));
 		anim.SetFloat ("WalkSpeedZ", Mathf.Abs (moveVertical));
 
+		anim.SetBool ("Running", run);
+		/*if (run = true)
+		{
+			speed *= 1.5f;
+		}*/
+
+
+
+
+
+		//Sets Gravity settings for if jumping. THIS CODE AND THE JUMPING ANIMATION CODE MIGHT BE ABLE TO GO TOGETHER SOMEHOW??
+		if (player1Control.isGrounded) 
+		{
+			verticalVelocity = -gravity * Time.deltaTime;
+			if (Input.GetKey(KeyCode.Space))
+			{
+				verticalVelocity = jumpForce;
+			}
+			else
+			{
+				verticalVelocity -= gravity * Time.deltaTime;
+			}
+		}
+
+
+
+
+
+
+
 		// Flip Player Over //
 		turn (moveHorizontal);
 
-		if(
 	}
-
-
-
-
-
-
-
-
+		
 
 	//code 	for turning the player to either right or left.
 	private void turn(float moveHorizontal)
